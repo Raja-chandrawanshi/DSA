7e2b091dc47160a9578b97db328cf895b5fd2264
Raja-chandrawanshi <rajachandrawanshi96@gmail.com>
2023-04-15 02:43:32 +0530

    check Peak element

--- Binary_Search/CheckPeakElement.js
+++ Binary_Search/CheckPeakElement.js
@@ -1,4 +1,6 @@
-let arr = [7, 9, 11, 13, 1, 2, 4, 5, 8, 11];
+//Find Peak Element
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 1];
 
 function CheckPeakElement(arr) {
   start = 0;
@@ -12,10 +14,7 @@ function CheckPeakElement(arr) {
       end = mid;
     }
 
-    if (
-      (mid === start && arr[mid] > arr[mid + 1]) ||
-      (mid === end && arr[mid] > arr[mid - 1])
-    ) {
+    if (mid === start && arr[mid] > arr[mid + 1]) {
       return arr[mid];
     }
   }
